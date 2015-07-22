205b4b56fd38cb8cd0b014646584feda4edc9950
Markus Malkusch <markus@malkusch.de>
2015-07-22 22:39:41 +0200

    initial documentation

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","malkusch\\lock\\CASMutex"],["c","malkusch\\lock\\DoubleCheckedLocking"],["c","malkusch\\lock\\exception\\LockAcquireException"],["c","malkusch\\lock\\exception\\LockReleaseException"],["c","malkusch\\lock\\exception\\MutexException"],["c","malkusch\\lock\\FlockMutex"],["c","malkusch\\lock\\MemcacheMutex"],["c","malkusch\\lock\\Mutex"],["c","malkusch\\lock\\NoMutex"],["c","malkusch\\lock\\SemaphoreMutex"],["c","malkusch\\lock\\TransactionalMutex"]];
